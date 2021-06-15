5ea84e3f3658709f3c5342e1a33a1a45a58f8efc
ballesteros.bermejo.adria <ballesteros.bermejo.adria@alumnat.copernic.cat>
2021-06-15 16:55:08 +0200

    first commit

---
+++ ajax.php
@@ -0,0 +1,50 @@
+
+<?php
+    $modalidad = $_GET['modalidad'];
+
+    $conn = mysqli_connect("localhost:3306", "root", "abc.123", "m07uf3");
+    if (!$conn) {
+        die('Could not connect: ' . mysqli_error($con));
+    }
+
+    if($modalidad != "ninguno"){
+        $sql = "SELECT * FROM estadistiques WHERE modalitat = '" . $modalidad . "'";
+    }else{
+        $sql = "SELECT * FROM estadistiques";
+    }
+    
+    $result = mysqli_query($conn, $sql);
+
+    echo "<table id='ta'>
+            <tr>
+            <th>Id</th>
+            <th>Modalitat</th>
+            <th>Nivell</th>
+            <th>Data_Partida</th>
+            <th>Intents</th>
+            </tr>";
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['modalitat'] . "</td>";
+        echo "<td>" . $row['nivell'] . "</td>";
+        echo "<td>" . $row['data_partida'] . "</td>";
+        echo "<td>" . $row['intents'] . "</td>";
+        echo "<td>
+                <form method='POST'>
+                    <input type='hidden' name='encontrar' value='". $row['id'] ."'>
+                    <input type='submit' value='Modificar' formaction='./modificar.php'>
+                </form>
+              </td>";
+        echo "<td>
+                <form method='POST'>
+                    <input type='hidden' name='borrar' value='". $row['id'] ."'>
+                    <input type='submit' value='Borrar' formaction='./borrar.php'>
+                </form>
+              </td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    mysqli_close($conn);
+?>
+
---
+++ borrar.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'pep/DatabaseOOP.php';
+
+    try{
+        $conn = new DatabaseOOP("localhost:3306", "root", "abc.123", "m07uf3");
+        $conn->connect();
+
+        //funcion borrar
+        $conn->delete($_POST["borrar"]);
+
+        //Header para la redireccion
+        header("Location: ./estadisticas.php");
+    } catch (Exception $error) {
+        echo "connection failed: " . $error->getMessage();
+    }
+    DatabaseProc::TABLE_END
+?>
